74ab88f5b4ffcfb65fac897da2f154703abcd82e
shipdonveseli <veseli.shipdon@gmail.com>
2022-09-21 18:46:59 +0200

    updated prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,43 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-preset-env",
+                      {
+                        // Options
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        }
+      ],
+    },
   // 1
   // Use the src/index.js file as entry point to bundle it.
   // If the src/index.js file imports other JS files,
